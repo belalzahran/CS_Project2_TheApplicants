54861a9417c11752d980716439952c620c8a64b9
mcdipples <Sarebear323@gmail.com>
2022-05-02 23:51:42 -0700

    Dijkstra's functional, sierrasDijkstras returns the value from source to destination to be used for display

--- CollegeTouring/main.cpp
+++ CollegeTouring/main.cpp
@@ -20,10 +20,11 @@ int main(int argc, char *argv[])
     DBColleges::getInstance().populateGraph(); //Populates the graph based on the collegeMap
 
     //DBColleges::getInstance().collegesGraph.minimumSpanningTree(0);
-    // DBColleges::getInstance().collegesGraph.graphDijkstras(4);
+   // DBColleges::getInstance().collegesGraph.graphDijkstras(4, 6);
     //DBColleges::getInstance().collegesGraph.PerformDFS(6);
+    DBColleges::getInstance().collegesGraph.sierrasDijkstras(4, 6);
 
-    MainWindow w;
-    w.show();
+//    MainWindow w;
+//    w.show();
     return a.exec();
 }
